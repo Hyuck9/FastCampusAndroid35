027109e1e2258562c7b03f78c0f4c1df2ead1967
Hyuck <lhg1304@nexmore.co.kr>
2023-02-26 11:50:37 +0900

    part4-chapter3-3 init

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-3/app/src/main/java/kr/co/fastcampus/part4/chapter3_3/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-3/app/src/main/java/kr/co/fastcampus/part4/chapter3_3/MainActivity.kt
@@ -4,25 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import kr.co.fastcampus.part4.chapter3_3.ui.theme.Part4chapter33Theme
+import kr.co.fastcampus.part4.chapter3_3.ui.theme.ButtonTheme
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			Part4chapter33Theme {
+			ButtonTheme {
 				// A surface container using the 'background' color from the theme
 				Surface(
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					ButtonExample(onButtonClicked = {})
 				}
 			}
 		}
@@ -30,14 +31,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+fun ButtonExample(onButtonClicked: () -> Unit) {
+	Button(onClick = {}) {
+		Text(text = "Send")
+	}
+
+	// 스탭 1: Button을 클릭했을 때 Toast를 출력하게 만들어봅시다.
+
+	// 스탭 2: Icon을 Text 앞에 추가시켜봅시다.
+	// imageVector에는 Icons.Filled.Send를 넣고
+	// contentDescription에는 null을 넣어봅시다.
+
+	// 스탭 3: 아이콘과 텍스트 사이에 Spacer를 넣어봅시다.
+	// modifier에 Modifier.size를 넣고 사이즈를
+	// ButtonDefaults.IconSpacing을 지정합시다.
+
+	// 스탭 4: enabled를 false로 바꾸어봅시다.
+
+	// 스탭 5: border에 BorderStroke를 설정합시다.
+
+	// 스탭 6: shape를 CircleShape로 지정합시다.
+
+	// 스탭 7: contentPadding에 PaddingValues를 설정합시다.
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-	Part4chapter33Theme {
-		Greeting("Android")
+	ButtonTheme {
+		ButtonExample(onButtonClicked = {})
 	}
 }
