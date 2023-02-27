dfa10601a08b1f929757e918e4f19aaa5be7bade
Hyuck <lhg1304@nexmore.co.kr>
2023-02-27 22:38:58 +0900

    part4-chapter3-3 Button 컴포넌트

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-3/app/src/main/java/kr/co/fastcampus/part4/chapter3_3/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-3/app/src/main/java/kr/co/fastcampus/part4/chapter3_3/MainActivity.kt
@@ -1,16 +1,24 @@
 package kr.co.fastcampus.part4.chapter3_3
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kr.co.fastcampus.part4.chapter3_3.ui.theme.ButtonTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +31,9 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					ButtonExample(onButtonClicked = {})
+					ButtonExample(onButtonClicked = {
+						Toast.makeText(this, "Send clicked.", Toast.LENGTH_SHORT).show()
+					})
 				}
 			}
 		}
@@ -32,27 +42,95 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ButtonExample(onButtonClicked: () -> Unit) {
-	Button(onClick = {}) {
-		Text(text = "Send")
-	}
+//	Button(onClick = {}) {
+//		Text(text = "Send")
+//	}
 
 	// 스탭 1: Button을 클릭했을 때 Toast를 출력하게 만들어봅시다.
+//	Button(onClick = onButtonClicked) {
+//		Text(text = "Send")
+//	}
 
 	// 스탭 2: Icon을 Text 앞에 추가시켜봅시다.
 	// imageVector에는 Icons.Filled.Send를 넣고
 	// contentDescription에는 null을 넣어봅시다.
+//	Button(onClick = onButtonClicked) {
+//		Icon(
+//			imageVectors = Icons.Filled.Send,
+//			contentDescription = null
+//		)
+//		Text(text = "Send")
+//	}
 
 	// 스탭 3: 아이콘과 텍스트 사이에 Spacer를 넣어봅시다.
 	// modifier에 Modifier.size를 넣고 사이즈를
 	// ButtonDefaults.IconSpacing을 지정합시다.
+//	Button(onClick = onButtonClicked) {
+//		Icon(
+//			imageVectors = Icons.Filled.Send,
+//			contentDescription = null
+//		)
+//		Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+//		Text(text = "Send")
+//	}
 
 	// 스탭 4: enabled를 false로 바꾸어봅시다.
+//	Button(
+//		onClick = onButtonClicked,
+//		enabled = false
+//	) {
+//		Icon(
+//			imageVectors = Icons.Filled.Send,
+//			contentDescription = null
+//		)
+//		Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+//		Text(text = "Send")
+//	}
 
 	// 스탭 5: border에 BorderStroke를 설정합시다.
+//	Button(
+//		onClick = onButtonClicked,
+//		enabled = true,
+//		border = BorderStroke(10.dp, Color.Magenta)
+//	) {
+//		Icon(
+//			imageVectors = Icons.Filled.Send,
+//			contentDescription = null
+//		)
+//		Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+//		Text(text = "Send")
+//	}
 
 	// 스탭 6: shape를 CircleShape로 지정합시다.
+//	Button(
+//		onClick = onButtonClicked,
+//		enabled = true,
+//		border = BorderStroke(10.dp, Color.Magenta),
+//		shape = CircleShape
+//	) {
+//		Icon(
+//			imageVectors = Icons.Filled.Send,
+//			contentDescription = null
+//		)
+//		Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+//		Text(text = "Send")
+//	}
 
 	// 스탭 7: contentPadding에 PaddingValues를 설정합시다.
+//	Button(
+//		onClick = onButtonClicked,
+//		enabled = true,
+//		border = BorderStroke(10.dp, Color.Magenta),
+//		shape = CircleShape,
+//		contentPadding = PaddingValues(20.dp)
+//	) {
+//		Icon(
+//			imageVectors = Icons.Filled.Send,
+//			contentDescription = null
+//		)
+//		Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+//		Text(text = "Send")
+//	}
 }
 
 @Preview(showBackground = true)
