8f94d484b4ef9bffde631bd4fdb16dee0b7a32e2
Hyuck <lhg1304@nexmore.co.kr>
2023-03-03 22:53:31 +0900

    part4-chapter3-5 Surface

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-5/app/src/main/java/kr/co/fastcampus/part4/chapter3_5/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-5/app/src/main/java/kr/co/fastcampus/part4/chapter3_5/MainActivity.kt
@@ -3,13 +3,17 @@ package kr.co.fastcampus.part4.chapter3_5
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kr.co.fastcampus.part4.chapter3_5.ui.theme.SurfaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,13 +21,7 @@ class MainActivity : ComponentActivity() {
 		super.onCreate(savedInstanceState)
 		setContent {
 			SurfaceTheme {
-				// A surface container using the 'background' color from the theme
-				Surface(
-					modifier = Modifier.fillMaxSize(),
-					color = MaterialTheme.colorScheme.background
-				) {
-					Greeting("Android")
-				}
+				Greeting("Android")
 			}
 		}
 	}
@@ -31,7 +29,76 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+//	Surface(
+//		modifier = Modifier.padding(5.dp)
+//	) {
+//		Text(
+//			text = "Hello $name!",
+//			modifier = Modifier.padding(8.dp)
+//		)
+//	}
+
+	// 스텝 1: Surface에 elevation을 설정합시다.
+//	Surface(
+//		modifier = Modifier.padding(5.dp),
+//		shadowElevation = 10.dp
+//	) {
+//		Text(
+//			text = "Hello $name!",
+//			modifier = Modifier.padding(8.dp)
+//		)
+//	}
+
+	// 스텝 2: border의 값을 설정해봅시다.
+//	Surface(
+//		border = BorderStroke(
+//			width = 2.dp,
+//			color = Color.Magenta
+//		),
+//		modifier = Modifier.padding(5.dp),
+//		shadowElevation = 10.dp
+//	) {
+//		Text(
+//			text = "Hello $name!",
+//			modifier = Modifier.padding(8.dp)
+//		)
+//	}
+
+	// 스텝 3: Surface의 shape도 설정해봅시다.
+//	Surface(
+//		border = BorderStroke(
+//			width = 2.dp,
+//			color = Color.Magenta
+//		),
+//		modifier = Modifier.padding(5.dp),
+//		shadowElevation = 10.dp,
+//		shape = CircleShape
+//	) {
+//		Text(
+//			text = "Hello $name!",
+//			modifier = Modifier.padding(8.dp)
+//		)
+//	}
+
+	// 스텝 4: color를 지정합시다.
+	// MaterialTheme.colors에서 primary, error,
+	// background, surface, secondary 등을 지정해봅시다.
+	// contentColor가 자동으로 선택됩니다.
+	Surface(
+		border = BorderStroke(
+			width = 2.dp,
+			color = Color.Magenta
+		),
+		modifier = Modifier.padding(5.dp),
+		shadowElevation = 10.dp,
+		shape = CircleShape,
+		color = MaterialTheme.colorScheme.error
+	) {
+		Text(
+			text = "Hello $name!",
+			modifier = Modifier.padding(8.dp)
+		)
+	}
 }
 
 @Preview(showBackground = true)
