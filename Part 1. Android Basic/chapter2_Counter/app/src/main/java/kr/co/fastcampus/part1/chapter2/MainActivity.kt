689dd3704309cb6d2e056e721289372733ee4e46
Hyuck <lhg1304@nexmore.co.kr>
2023-03-05 11:05:31 +0900

    part1-chapter2 05.유저 입력값 받아 오기

--- Part 1. Android Basic/chapter2_Counter/app/src/main/java/kr/co/fastcampus/part1/chapter2/MainActivity.kt
+++ Part 1. Android Basic/chapter2_Counter/app/src/main/java/kr/co/fastcampus/part1/chapter2/MainActivity.kt
@@ -2,10 +2,20 @@ package kr.co.fastcampus.part1.chapter2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+		val numberTextView = findViewById<TextView>(R.id.numberTextView)
+		val resetButton = findViewById<Button>(R.id.resetButton)
+		val plusButton = findViewById<Button>(R.id.plusButton)
+
+		resetButton.setOnClickListener { Log.d("onClick", "리셋 버튼 클릭") }
+		plusButton.setOnClickListener { Log.d("onClick", "플러스 버튼 클릭") }
 	}
 }
