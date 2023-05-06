e4b82f07191531f2bada37d0d673fbc82d6d451b
Hyuck <lhg1304@nexmore.co.kr>
2023-05-06 11:26:55 +0900

    part4-chapter3-12 init

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-12/app/src/main/java/kr/co/fastcampus/part4/chapter3_12/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-12/app/src/main/java/kr/co/fastcampus/part4/chapter3_12/MainActivity.kt
@@ -3,41 +3,84 @@ package kr.co.fastcampus.part4.chapter3_12
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import kr.co.fastcampus.part4.chapter3_12.ui.theme.Part4chapter312Theme
+import androidx.compose.ui.unit.dp
+import kr.co.fastcampus.part4.chapter3_12.ui.theme.CardTheme
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			Part4chapter312Theme {
+			CardTheme {
 				// A surface container using the 'background' color from the theme
 				Surface(
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					Column {
+						CardEx(cardData)
+						CardEx(cardData)
+					}
 				}
 			}
 		}
 	}
+
+	companion object {
+		val cardData = CardData(
+			imageUri = "https://raw.githubusercontent.com/Fastcampus-Android-Lecture-Project-2023/part4-chapter3/main/part4-chapter3-10/app/src/main/res/drawable-xhdpi/wall.jpg",
+			imageDescription = "엔텔로프 캐년",
+			author = "Dalinaum",
+			description = "엔텔로프 캐년은 죽기 전에 꼭 봐야할 절경으로 소개되었습니다."
+		)
+	}
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+fun CardEx(cardData: CardData) {
+	val placeHolderColor = Color(0x33000000)
+
+	Card(
+		elevation = CardDefaults.cardElevation(8.dp),
+		modifier = Modifier.padding(4.dp),
+	) {
+		Row(
+			verticalAlignment = Alignment.CenterVertically,
+			modifier = Modifier.padding(8.dp)
+		) {
+			// 스텝 1: `AsyncImage`, `Spacer`, `Column`, `Text`로
+			// 레이아웃을 만들어보세요.
+
+			// 스텝 2: `AsyncImage`에는 `placeholder`를 지정하고,
+			// `contentScale`을 `ContentScale.Crop`으로 설정합시다.
+			// `clip(CircleShape)`로 둥근 외양을 만들어 봅시다.
+		}
+	}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-	Part4chapter312Theme {
-		Greeting("Android")
+	CardTheme {
+		Row {
+			CardEx(MainActivity.cardData)
+		}
 	}
 }
+
+data class CardData(
+	val imageUri: String,
+	val imageDescription: String,
+	val author: String,
+	val description: String
+)
