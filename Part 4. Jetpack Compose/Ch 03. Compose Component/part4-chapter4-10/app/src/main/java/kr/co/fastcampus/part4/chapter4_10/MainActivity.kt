5a20dc5860a186aee975ef5935651dfe4d9ac388
Hyuck <lhg1304@nexmore.co.kr>
2023-05-06 22:27:54 +0900

    part4-chapter4-10 init

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-10/app/src/main/java/kr/co/fastcampus/part4/chapter4_10/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-10/app/src/main/java/kr/co/fastcampus/part4/chapter4_10/MainActivity.kt
@@ -3,26 +3,28 @@ package kr.co.fastcampus.part4.chapter4_10
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import kr.co.fastcampus.part4.chapter4_10.ui.theme.Part4chapter410Theme
+import kr.co.fastcampus.part4.chapter4_10.ui.theme.DropDownMenuTheme
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			Part4chapter410Theme {
+			DropDownMenuTheme {
 				// A surface container using the 'background' color from the theme
 				Surface(
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					DropDownMenuEx()
 				}
 			}
 		}
@@ -30,14 +32,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+fun DropDownMenuEx() {
+	var expandDropDownMenu by remember { mutableStateOf(false) }
+	var counter by remember { mutableStateOf(0) }
+
+	Column {
+		Button(onClick = { expandDropDownMenu = true }) {
+			Text("드롭다운 메뉴 열기")
+		}
+		Text("카운터: $counter")
+	}
+
+	// 단계 1: `DropdownMenu`를 만들고 `expanded`를 `expandDropDownMenu`로
+	// 등록합시다.
+	// `onDismissRequest`에 대해서는 `expandDropDownMenu`를 `false`로 바꿉니다.
+
+	// 단계 2: 두개의 `DropdownMenuItem`을 등록합시다. `onClick`을 구현하고
+	// 내용물은 `Text`로 채워봅시다.
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-	Part4chapter410Theme {
-		Greeting("Android")
+	DropDownMenuTheme {
+		DropDownMenuEx()
 	}
 }
