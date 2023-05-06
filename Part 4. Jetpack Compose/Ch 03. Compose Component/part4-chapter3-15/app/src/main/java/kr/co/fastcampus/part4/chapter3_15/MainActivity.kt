49a9877f5f54250c921ba5753dc7ac577754377f
Hyuck <lhg1304@nexmore.co.kr>
2023-05-06 15:49:15 +0900

    part4-chapter3-15 init

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-15/app/src/main/java/kr/co/fastcampus/part4/chapter3_15/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-15/app/src/main/java/kr/co/fastcampus/part4/chapter3_15/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.fastcampus.part4.chapter3_15
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,19 +11,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import kr.co.fastcampus.part4.chapter3_15.ui.theme.Part4chapter315Theme
+import kr.co.fastcampus.part4.chapter3_15.ui.theme.TopAppBarTheme
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			Part4chapter315Theme {
+			TopAppBarTheme {
 				// A surface container using the 'background' color from the theme
 				Surface(
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					TopBarEx("Android")
 				}
 			}
 		}
@@ -30,14 +31,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+fun TopBarEx(name: String) {
+	Column {
+		// 스텝 1: TopAppBar를 만들고 title 항목을 채워봅시다.
+
+		// 스텝 2: navigationIcon 파라미터를 채워봅시다.
+		// IconButton을 만들고 자식으로 Icon을 넣읍시다.
+		// 아이콘은 Icons.Filled.ArrowBack을 채웁시다.
+		// onClick은 비워둡시다.
+
+		// 스텝 3: actions를 추가해봅시다.
+		// Icons.Filled의 여러 아이콘을 이용해봅시다.
+
+		// 스텝 4: TopAppBar content 파라미터 버전을 만들어봅시다.
+
+		Text(text = "Hello $name!")
+	}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-	Part4chapter315Theme {
-		Greeting("Android")
+	TopAppBarTheme {
+		TopBarEx("Android")
 	}
 }
