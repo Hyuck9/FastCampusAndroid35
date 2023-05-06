e672b6dd314ae5299723085a9d64cfc7a8e6880e
Hyuck <lhg1304@nexmore.co.kr>
2023-05-06 22:25:13 +0900

    part4-chapter4-9 Custom Dialog

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-09/app/src/main/java/kr/co/fastcampus/part4/chapter4_9/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-09/app/src/main/java/kr/co/fastcampus/part4/chapter4_9/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import kr.co.fastcampus.part4.chapter4_9.ui.theme.CustomDialogTheme
 
@@ -49,9 +52,32 @@ fun CustomDialog() {
 	if (openDialog) {
 		Dialog(onDismissRequest = {
 			// 단계 1: 디스미스 처리를 합니다.
+			openDialog = false
 		}) {
 			Surface {
 				// 단계 2: 컬럼을 만들고 설명을 적어봅시다.
+				Column(modifier = Modifier.padding(8.dp)) {
+					Text("버튼을 클릭해주세요.\n * +1을 누르면 값이 증가됩니다.\n * -1을 누르면 값이 감소합니다.")
+					Row {
+						Button(onClick = {
+							openDialog = false
+						}) {
+							Text(text = "취소")
+						}
+						Button(onClick = {
+							openDialog = false
+							counter++
+						}) {
+							Text(text = "+1")
+						}
+						Button(onClick = {
+							openDialog = false
+							counter--
+						}) {
+							Text(text = "-1")
+						}
+					}
+				}
 
 				// 단계 3: 컬럼 안에 로우를 만들어 수평 방향으로 버튼을 배열합니다.
 				// 버튼은 +1, -1, 취소로 구성하겠습니다.
