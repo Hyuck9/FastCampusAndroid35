e36ec4e0d4278a24b355caebce46c26631d2ad43
Hyuck <lhg1304@nexmore.co.kr>
2023-05-06 10:47:15 +0900

    part4-chapter3-10 Image

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-10/app/src/main/java/kr/co/fastcampus/part1/part4/part4/part4/part4/chapter3_10/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-10/app/src/main/java/kr/co/fastcampus/part1/part4/part4/part4/part4/chapter3_10/MainActivity.kt
@@ -3,13 +3,17 @@ package kr.co.fastcampus.part1.part4.part4.part4.part4.chapter3_10
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import kr.co.fastcampus.part1.part4.part4.part4.part4.chapter3_10.ui.theme.ImageTheme
 
@@ -36,13 +40,18 @@ fun Greeting() {
 		// 스텝 1: Image를 만들어봅시다.
 		// painter 항목에 painterResource(id = R.drawable.wall)
 		// contentDescription에 엔텔로프 캐년이라고 넣읍시다.
-
-		// Image
+		Image(
+			painter = painterResource(id = R.drawable.wall),
+			contentDescription = "엔텔로프 캐년"
+		)
 
 		// 스텝 2: 두 번째 Image를 만들어봅시다.
 		// imageVector에 Icons.Filled.Settings를 설정해봅시다.
 
-		// Image
+		Image(
+			imageVector = Icons.Filled.Settings,
+			contentDescription = "세팅"
+		)
 	}
 }
 
