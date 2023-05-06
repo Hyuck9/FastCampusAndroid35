3ac0cab71e29914a2a0480952e4e8b1d05c3f760
Hyuck <lhg1304@nexmore.co.kr>
2023-05-06 16:41:43 +0900

    part4-chapter3-18 init

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-18/app/src/main/java/kr/co/fastcampus/part4/chapter3_18/ui/theme/Theme.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-18/app/src/main/java/kr/co/fastcampus/part4/chapter3_18/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun Part4chapter318Theme(
+fun CatalogTheme(
 	darkTheme: Boolean = isSystemInDarkTheme(),
 	// Dynamic color is available on Android 12+
 	dynamicColor: Boolean = true,
