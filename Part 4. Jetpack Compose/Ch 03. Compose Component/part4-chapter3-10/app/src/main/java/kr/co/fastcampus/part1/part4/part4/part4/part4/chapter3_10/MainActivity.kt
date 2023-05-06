415bf39fdd248fddfb772c04b2aa016b345f90c6
Hyuck <lhg1304@nexmore.co.kr>
2023-05-06 10:39:12 +0900

    part4-chapter3-10 init

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-10/app/src/main/java/kr/co/fastcampus/part1/part4/part4/part4/part4/chapter3_10/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-10/app/src/main/java/kr/co/fastcampus/part1/part4/part4/part4/part4/chapter3_10/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.co.fastcampus.part1.part4.part4.part4.part4.chapter3_10
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,19 +11,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import kr.co.fastcampus.part1.part4.part4.part4.part4.chapter3_10.ui.theme.Part4chapter310Theme
+import kr.co.fastcampus.part1.part4.part4.part4.part4.chapter3_10.ui.theme.ImageTheme
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			Part4chapter310Theme {
+			ImageTheme {
 				// A surface container using the 'background' color from the theme
 				Surface(
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					Greeting()
 				}
 			}
 		}
@@ -30,14 +31,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+fun Greeting() {
+	Column {
+		// 스텝 1: Image를 만들어봅시다.
+		// painter 항목에 painterResource(id = R.drawable.wall)
+		// contentDescription에 엔텔로프 캐년이라고 넣읍시다.
+
+		// Image
+
+		// 스텝 2: 두 번째 Image를 만들어봅시다.
+		// imageVector에 Icons.Filled.Settings를 설정해봅시다.
+
+		// Image
+	}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-	Part4chapter310Theme {
-		Greeting("Android")
+	ImageTheme {
+		Greeting()
 	}
 }
--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-10/app/src/main/java/kr/co/fastcampus/part1/part4/part4/part4/part4/chapter3_10/ui/theme/Theme.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-10/app/src/main/java/kr/co/fastcampus/part1/part4/part4/part4/part4/chapter3_10/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun Part4chapter310Theme(
+fun ImageTheme(
 	darkTheme: Boolean = isSystemInDarkTheme(),
 	// Dynamic color is available on Android 12+
 	dynamicColor: Boolean = true,
