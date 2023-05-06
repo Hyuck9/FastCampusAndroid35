42dd87b11e2fb45e797003a7880535dc000f3d6d
Hyuck <lhg1304@nexmore.co.kr>
2023-05-06 15:47:01 +0900

    part4-chapter3-14 TextField

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-14/app/src/main/java/kr/co/fastcampus/part4/chapter3_14/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-14/app/src/main/java/kr/co/fastcampus/part4/chapter3_14/MainActivity.kt
@@ -3,13 +3,9 @@ package kr.co.fastcampus.part4.chapter3_14
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -34,23 +30,49 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
+	var name by remember { mutableStateOf("Tom") }
 	Column(modifier = Modifier.padding(16.dp)) {
 		// 스텝 1: TextField를 Text위에 만듭니다.
 		// value와 onValueChanged는 비워둡시다.
+//		TextField(value = "Tom", onValueChange = {})
 
 		// 스텝 2: Text에 Android 대신 TextField 입력을
 		// 출력하게 합시다. mutableStateOf("") 필드를
 		// 하나 만듭시다.
+//		TextField(value = name, onValueChange = { name = it })
 
 		// 스텝 3: TextField에 label을 추가합시다.
 		// 내용에는 `Text("Name")`을 채워봅시다.
+//		TextField(
+//			value = name,
+//			label = {
+//				Text(text = "이름")
+//			},
+//			onValueChange = { name = it }
+//		)
 
 		// 스텝 4: TextField와 Text 사이에 Spacer를 넣어 8.dp
 		// 간격을 줍시다.
+//		TextField(
+//			value = name,
+//			label = {
+//				Text(text = "이름")
+//			},
+//			onValueChange = { name = it }
+//		)
+//		Spacer(modifier = Modifier.size(8.dp))
 
 		// 스텝 5: TextField를 OutlinedTextField로 변경해봅시다.
+		OutlinedTextField(
+			value = name,
+			label = {
+				Text(text = "이름")
+			},
+			onValueChange = { name = it }
+		)
+		Spacer(modifier = Modifier.size(8.dp))
 
-		Text(text = "Hello Android")
+		Text(text = "Hello $name")
 	}
 }
 
