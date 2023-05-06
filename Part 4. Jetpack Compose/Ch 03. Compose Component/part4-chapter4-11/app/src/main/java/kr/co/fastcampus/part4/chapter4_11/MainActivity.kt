28931503be6b83a36f00e70816f85ea00ee53ccc
Hyuck <lhg1304@nexmore.co.kr>
2023-05-06 22:41:11 +0900

    part4-chapter4-11 init

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-11/app/src/main/java/kr/co/fastcampus/part4/chapter4_11/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-11/app/src/main/java/kr/co/fastcampus/part4/chapter4_11/MainActivity.kt
@@ -4,40 +4,61 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import kr.co.fastcampus.part4.chapter4_11.ui.theme.Part4chapter411Theme
+import kr.co.fastcampus.part4.chapter4_11.ui.theme.SnackbarTheme
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			Part4chapter411Theme {
+			SnackbarTheme {
 				// A surface container using the 'background' color from the theme
 				Surface(
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					SnackbarEx()
 				}
 			}
 		}
 	}
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+fun SnackbarEx() {
+	var counter by remember { mutableStateOf(0) }
+
+	// 단계 3: couroutineScope를 만듭시다.
+	// `rememberCoroutineScope`를 사용합니다.
+
+	// 단계 1: scaffoldState를 만들고 Scaffold에 설정합시다.
+	// scaffoldState를 만들기 위해 `rememberScaffoldState`를 사용합니다.
+	Scaffold {
+		Surface(modifier = Modifier.padding(it)) {}
+		// 단계 2: "더하기" 버튼을 만들어 봅시다.
+		// action에서 counter를 증가시킵시다.
+
+		// 단계 4: 버튼의 onClick에서 `coroutineScope.launch`를
+		// 사용합니다.
+
+		// 단계 5: 스낵바를 사용하기 위해 `scaffoldState.snackbarHostState.showSnackbar`
+		// 사용합니다.
+
+		// `message`에 카운터를 출력합시다.
+		// `actionLabel`를 "닫기"로 지정합시다.
+		// `duration`에 `SnackbarDuration.Short`를 사용합니다.
+	}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-	Part4chapter411Theme {
-		Greeting("Android")
+	SnackbarTheme {
+		SnackbarEx()
 	}
 }
