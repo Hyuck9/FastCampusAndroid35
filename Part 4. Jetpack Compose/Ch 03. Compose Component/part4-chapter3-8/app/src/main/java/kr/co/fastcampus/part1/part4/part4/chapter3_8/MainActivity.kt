080695a362b7e34c26b469b432850aae88a3b6bc
Hyuck <lhg1304@nexmore.co.kr>
2023-05-06 10:22:47 +0900

    part4-chapter3-8 Column

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-8/app/src/main/java/kr/co/fastcampus/part1/part4/part4/chapter3_8/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter3-8/app/src/main/java/kr/co/fastcampus/part1/part4/part4/chapter3_8/MainActivity.kt
@@ -3,26 +3,31 @@ package kr.co.fastcampus.part1.part4.part4.chapter3_8
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import kr.co.fastcampus.part1.part4.part4.chapter3_8.ui.theme.Part4chapter38Theme
+import androidx.compose.ui.unit.dp
+import kr.co.fastcampus.part1.part4.part4.chapter3_8.ui.theme.ColumnTheme
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			Part4chapter38Theme {
+			ColumnTheme {
 				// A surface container using the 'background' color from the theme
 				Surface(
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					ColumnEx("Android")
 				}
 			}
 		}
@@ -30,14 +35,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+fun ColumnEx(name: String) {
+//	Column(modifier = Modifier.size(100.dp)) {
+//		Text(text = "첫 번째")
+//		Text(text = "두 번째")
+//		Text(text = "세 번째")
+//	}
+
+	// 스텝 1: horizontalAlignment를 Column에 적용해봅시다.
+//	Column(
+//		horizontalAlignment = Alignment.End,
+//		modifier = Modifier.size(100.dp)
+//	) {
+//		Text(text = "첫 번째")
+//		Text(text = "두 번째")
+//		Text(text = "세 번째")
+//	}
+
+	// 스텝 2: Column에 verticalArrangement를 지정해봅시다.
+	// SpaceAround, SpaceEvenly, SpaceBetween도 해봅시다.
+//	Column(
+//		horizontalAlignment = Alignment.End,
+//		verticalArrangement = Arrangement.SpaceBetween,
+//		modifier = Modifier.size(100.dp)
+//	) {
+//		Text(text = "첫 번째")
+//		Text(text = "두 번째")
+//		Text(text = "세 번째")
+//	}
+
+	// 스텝 3: Text에 Modifier.align을 사용해 봅시다.
+	Column(
+		horizontalAlignment = Alignment.End,
+		verticalArrangement = Arrangement.SpaceBetween,
+		modifier = Modifier.size(100.dp)
+	) {
+		Text(
+			text = "첫 번째",
+			modifier = Modifier.align(Alignment.CenterHorizontally)
+		)
+		Text(text = "두 번째")
+		Text(
+			text = "세 번째",
+			modifier = Modifier.align(Alignment.Start)
+		)
+	}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-	Part4chapter38Theme {
-		Greeting("Android")
+	ColumnTheme {
+		ColumnEx("Android")
 	}
 }
