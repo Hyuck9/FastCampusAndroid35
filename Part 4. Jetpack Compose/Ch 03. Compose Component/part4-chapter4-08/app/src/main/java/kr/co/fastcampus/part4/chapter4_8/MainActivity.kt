4e2a1b2b521c8af719480cabca571ed8f62de8e9
Hyuck <lhg1304@nexmore.co.kr>
2023-05-06 22:14:12 +0900

    part4-chapter4-8 init

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-08/app/src/main/java/kr/co/fastcampus/part4/chapter4_8/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-08/app/src/main/java/kr/co/fastcampus/part4/chapter4_8/MainActivity.kt
@@ -3,26 +3,25 @@ package kr.co.fastcampus.part4.chapter4_8
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import kr.co.fastcampus.part4.chapter4_8.ui.theme.Part4chapter408Theme
+import kr.co.fastcampus.part4.chapter4_8.ui.theme.DialogTheme
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			Part4chapter408Theme {
+			DialogTheme {
 				// A surface container using the 'background' color from the theme
 				Surface(
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					DialogEx()
 				}
 			}
 		}
@@ -30,14 +29,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+fun DialogEx() {
+	var openDialog by remember { mutableStateOf(false) }
+	var counter by remember { mutableStateOf(0) }
+
+	Column {
+		Button(onClick = { openDialog = !openDialog }) {
+			Text("다이얼로그 열기")
+		}
+		Text("카운터: $counter")
+	}
+
+	if (openDialog) {
+		AlertDialog(onDismissRequest = {
+			// 단계 1: `openDialog`를 이용해 다이얼로그를 끌 수 있게 합니다.
+		}, confirmButton = {
+			// 단계 2: "더하기" 버튼을 만들고 `counter`를 증가시킵니다.
+			// 다이얼로그도 끕니다.
+		}, dismissButton = {
+			// 단계 3: "취소" 버튼을 만들고 다이얼로그를 끕니다.
+		}, title = {
+			// 단계 4: 타이틀을 만듭니다. "더하기" 정도로 해봅시다.
+		}, text = {
+			// 단계 5: 다이얼로그에서 설명할 문구를 출력합니다.
+		})
+	}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-	Part4chapter408Theme {
-		Greeting("Android")
+	DialogTheme {
+		DialogEx()
 	}
 }
--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-08/app/src/main/java/kr/co/fastcampus/part4/chapter4_8/ui/theme/Theme.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-08/app/src/main/java/kr/co/fastcampus/part4/chapter4_8/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun Part4chapter408Theme(
+fun DialogTheme(
 	darkTheme: Boolean = isSystemInDarkTheme(),
 	// Dynamic color is available on Android 12+
 	dynamicColor: Boolean = true,
