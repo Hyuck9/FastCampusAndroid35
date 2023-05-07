89d3896d6adaedb9e4e9cad232bc86452da9a005
Hyuck <lhg1304@nexmore.co.kr>
2023-05-07 10:24:35 +0900

    part4-chapter4-12 init

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-12/app/src/main/java/kr/co/fastcampus/part4/chapter4_12/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-12/app/src/main/java/kr/co/fastcampus/part4/chapter4_12/MainActivity.kt
@@ -8,21 +8,22 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import kr.co.fastcampus.part4.chapter4_12.ui.theme.Part4chapter412Theme
+import kr.co.fastcampus.part4.chapter4_12.ui.theme.BottomAppBarTheme
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			Part4chapter412Theme {
+			BottomAppBarTheme {
 				// A surface container using the 'background' color from the theme
 				Surface(
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					BottomAppBarEx()
 				}
 			}
 		}
@@ -30,14 +31,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+fun BottomAppBarEx() {
+//	val scaffoldState = rememberScaffoldState()
+	val coroutineScope = rememberCoroutineScope()
+
+	// 단계 1: `Scaffold`에 `scaffoldState`를 설정합니다.
+
+	// 단계 2: `bottomBar` 파라미터에 `BottomAppBar`를 넣읍시다.
+	// 내용은 텍스트와 버튼을 넣어 봅시다. 버튼에는 `snackBar`를
+	// 연동해 메시지를 출력합니다.
+
+	// 단계 3: 더하기와 빼기 버튼을 추가로 만들고 `MutableState`
+	// 만듭시다. `Scaffold`의 `content`에 `Text`를 넣어 카운터를 출력하게
+	// 합시다.
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-	Part4chapter412Theme {
-		Greeting("Android")
+	BottomAppBarTheme {
+		BottomAppBarEx()
 	}
 }
