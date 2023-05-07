e00d0fea4f3e42f80b3ba1c1a1da6fc66a5dbf09
Hyuck <lhg1304@nexmore.co.kr>
2023-05-07 10:39:16 +0900

    part4-chapter4-13 init

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-13/app/src/main/java/kr/co/fastcampus/part4/chapter4_13/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-13/app/src/main/java/kr/co/fastcampus/part4/chapter4_13/MainActivity.kt
@@ -3,26 +3,30 @@ package kr.co.fastcampus.part4.chapter4_13
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import kr.co.fastcampus.part4.chapter4_13.ui.theme.Part4chapter413Theme
+import androidx.compose.ui.unit.dp
+import kr.co.fastcampus.part4.chapter4_13.ui.theme.StateTheme
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			Part4chapter413Theme {
+			StateTheme {
 				// A surface container using the 'background' color from the theme
 				Surface(
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					PyeongToSquareMeter()
 				}
 			}
 		}
@@ -30,14 +34,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+fun PyeongToSquareMeter() {
+	var pyeong by remember {
+		mutableStateOf("23")
+	}
+	var squaremeter by remember {
+		mutableStateOf((23 * 3.306).toString())
+	}
+
+	// 단계 1: remember를 이용해 상태를 만들고 평 값을 입력하면
+	// 제곱미터가 출력되도록 화면을 구성하시오.
+	// 평을 제곱미터로 바꾸기 위해서는 3.306을 곱하면 됩니다.
+	Column(modifier = Modifier.padding(16.dp)) {
+		OutlinedTextField(
+			value = "",
+			onValueChange = {
+			}, label = {
+				Text("평")
+			}
+		)
+		OutlinedTextField(
+			value = "",
+			onValueChange = {},
+			label = {
+				Text("제곱미터")
+			}
+		)
+	}
+
+	// 단계 2: `Composable` 함수를 만들고 `Column`의 항목들을 옮기세요.
+	// 단 상태는 옮기지 말아야 합니다.
+
+	// 파라미터는 아래와 같이 구성합니다.
+	// `pyeong: String, squareMeter: String, onPyeongChange: (String) -> Unit`
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-	Part4chapter413Theme {
-		Greeting("Android")
+	StateTheme {
+		PyeongToSquareMeter()
 	}
 }
--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-13/app/src/main/java/kr/co/fastcampus/part4/chapter4_13/ui/theme/Theme.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-13/app/src/main/java/kr/co/fastcampus/part4/chapter4_13/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun Part4chapter413Theme(
+fun StateTheme(
 	darkTheme: Boolean = isSystemInDarkTheme(),
 	// Dynamic color is available on Android 12+
 	dynamicColor: Boolean = true,
