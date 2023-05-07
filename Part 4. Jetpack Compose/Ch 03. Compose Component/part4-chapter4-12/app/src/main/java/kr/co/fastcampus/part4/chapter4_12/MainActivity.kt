4e97089143985e995134d7f82ae7d85cd7dd4483
Hyuck <lhg1304@nexmore.co.kr>
2023-05-07 10:35:53 +0900

    part4-chapter4-12 BottomAppBar

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-12/app/src/main/java/kr/co/fastcampus/part4/chapter4_12/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-12/app/src/main/java/kr/co/fastcampus/part4/chapter4_12/MainActivity.kt
@@ -3,14 +3,15 @@ package kr.co.fastcampus.part4.chapter4_12
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
 import kr.co.fastcampus.part4.chapter4_12.ui.theme.BottomAppBarTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,12 +31,54 @@ class MainActivity : ComponentActivity() {
 	}
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BottomAppBarEx() {
-//	val scaffoldState = rememberScaffoldState()
+	val snackbarHostState = remember { SnackbarHostState() }
 	val coroutineScope = rememberCoroutineScope()
+	var counter by remember { mutableStateOf(0) }
 
 	// 단계 1: `Scaffold`에 `scaffoldState`를 설정합니다.
+	Scaffold(
+		snackbarHost = {
+			SnackbarHost(
+				hostState = snackbarHostState
+			)
+		},
+		bottomBar = {
+			BottomAppBar {
+				Text(text = "헬로")
+				Button(onClick = {
+					coroutineScope.launch {
+						snackbarHostState.showSnackbar(
+							"안녕하세요"
+						)
+					}
+				}) {
+					Text(text = "인사하기")
+				}
+				Button(onClick = {
+					counter++
+				}) {
+					Text(text = "더하기")
+				}
+				Button(onClick = {
+					counter--
+				}) {
+					Text(text = "빼기")
+				}
+			}
+		}
+	) {
+		Surface(modifier = Modifier.padding(it)) {
+			Box(modifier = Modifier.fillMaxSize()) {
+				Text(
+					text = "카운터는 ${counter}회입니다.",
+					modifier = Modifier.align(Alignment.Center)
+				)
+			}
+		}
+	}
 
 	// 단계 2: `bottomBar` 파라미터에 `BottomAppBar`를 넣읍시다.
 	// 내용은 텍스트와 버튼을 넣어 봅시다. 버튼에는 `snackBar`를
