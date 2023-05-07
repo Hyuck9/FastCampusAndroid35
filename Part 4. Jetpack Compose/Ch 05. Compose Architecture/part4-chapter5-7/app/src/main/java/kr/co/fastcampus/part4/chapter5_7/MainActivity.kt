41b1ad9001fc8ba1183a3b1d961b03484d9d9c4f
Hyuck <lhg1304@nexmore.co.kr>
2023-05-07 19:29:40 +0900

    part4-chapter5-7 init

--- Part 4. Jetpack Compose/Ch 05. Compose Architecture/part4-chapter5-7/app/src/main/java/kr/co/fastcampus/part4/chapter5_7/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 05. Compose Architecture/part4-chapter5-7/app/src/main/java/kr/co/fastcampus/part4/chapter5_7/MainActivity.kt
@@ -4,25 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import kr.co.fastcampus.part4.chapter5_7.ui.theme.Part4chapter57Theme
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kr.co.fastcampus.part4.chapter5_7.ui.theme.DITheme
+import kr.co.fastcampus.part4.chapter5_7.viewmodel.GithubViewModel
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			Part4chapter57Theme {
+			DITheme {
 				// A surface container using the 'background' color from the theme
 				Surface(
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					ReposScreen()
 				}
 			}
 		}
@@ -30,14 +35,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+fun ReposScreen(viewModel: GithubViewModel = viewModel()) {
+	LazyColumn {
+		item {
+			Button(onClick = {
+				viewModel.getRepos()
+			}) {
+				Text("리포지토리 가져오기")
+			}
+		}
+		items(viewModel.repos) {
+			Text(it.name)
+		}
+	}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-	Part4chapter57Theme {
-		Greeting("Android")
+	DITheme {
+		ReposScreen()
 	}
 }
---
+++ Part 4. Jetpack Compose/Ch 05. Compose Architecture/part4-chapter5-7/app/src/main/java/kr/co/fastcampus/part4/chapter5_7/model/Repo.kt
@@ -0,0 +1,9 @@
+package kr.co.fastcampus.part4.chapter5_7.model
+
+data class Repo(
+	val id: Long,
+	val name: String,
+	val htmlUrl: String,
+	val url: String,
+	val gitUrl: String
+)
