7eab5a188640c42e2e41479208081f4d4cb17a45
Hyuck <lhg1304@nexmore.co.kr>
2023-05-07 11:19:52 +0900

    part4-chapter4-14 애니메이션(2)

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-15/app/src/main/java/kr/co/fastcampus/part4/chapter4_15/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-15/app/src/main/java/kr/co/fastcampus/part4/chapter4_15/MainActivity.kt
@@ -3,26 +3,34 @@ package kr.co.fastcampus.part4.chapter4_15
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import kr.co.fastcampus.part4.chapter4_15.ui.theme.Part4chapter415Theme
+import androidx.compose.ui.unit.dp
+import kr.co.fastcampus.part4.chapter4_15.ui.theme.Animation2Theme
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContent {
-			Part4chapter415Theme {
+			Animation2Theme {
 				// A surface container using the 'background' color from the theme
 				Surface(
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colorScheme.background
 				) {
-					Greeting("Android")
+					Animation2Ex()
 				}
 			}
 		}
@@ -30,14 +38,161 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+fun Animation2Ex() {
+	var isDarkMode by remember { mutableStateOf(false) }
+
+	// 단계 1: `updateTransition` 수행하고 `targetState`를 `isDarkMode`로
+	// 설정합니다. `transition`으로 리턴을 받습니다.
+	val transition = updateTransition(targetState = isDarkMode, label = "다크 모드 트랜지션")
+
+	// 단계 2: `transition`에 대해 `animateColor`를 호출해 `backgroundColor`를 받습니다.
+	// 배경색상을 만듭시다. false일 때 하얀 배경, true일 때 검은 배경.
+	val backgroundColor by transition.animateColor(label = "다크 모드 배경색상 애니메이션") { isDark ->
+		when (isDark) {
+			true -> Color.Black
+			false -> Color.White
+		}
+	}
+
+	// 단계 3: 글자 색상을 만듭시다.
+	val textColor by transition.animateColor(label = "다크 모드 글자 색상 애니메이션") { isDark ->
+		when (isDark) {
+			true -> Color.White
+			false -> Color.Black
+		}
+	}
+
+	// 단계 4: `animateFloat`를 호출해서 알파 값을 만듭시다.
+	val alpha by transition.animateFloat(label = "다크 모드 알파 애니메이션") { isDark ->
+		when (isDark) {
+			true -> 1f
+			false -> 0.7f
+		}
+	}
+
+	// 단계 5: 컬럼에 배경과 알파를 적용합시다.
+	Column(
+		modifier = Modifier
+			.background(backgroundColor)
+			.alpha(alpha)
+	) {
+		// 단계 6: 라디오 버튼에 글자 색을 적용합시다.
+		RadioButtonWithText(
+			text = "일반 모드",
+			color = textColor,
+			selected = !isDarkMode
+		) {
+			isDarkMode = false
+		}
+		RadioButtonWithText(
+			text = "다크 모드",
+			color = textColor,
+			selected = isDarkMode
+		) {
+			isDarkMode = true
+		}
+
+		// 단계 7: Crossfade를 이용해 `isDarkMode`가 참일 경우
+		// `Row`로 항목을 표현하고 거짓일 경우 `Column`으로 표현해봅시다.
+//		Row {
+//			Box(modifier = Modifier
+//				.background(Color.Red)
+//				.size(20.dp)) {
+//				Text("1")
+//			}
+//			Box(modifier = Modifier
+//				.background(Color.Magenta)
+//				.size(20.dp)) {
+//				Text("2")
+//			}
+//			Box(modifier = Modifier
+//				.background(Color.Blue)
+//				.size(20.dp)) {
+//				Text("3")
+//			}
+//		}
+		Crossfade(targetState = isDarkMode) { state ->
+			 when (state) {
+				 false -> {
+					 Column {
+						 Box(modifier = Modifier
+							 .background(Color.Red)
+							 .size(20.dp)) {
+							 Text("1")
+						 }
+						 Box(modifier = Modifier
+							 .background(Color.Magenta)
+							 .size(20.dp)) {
+							 Text("2")
+						 }
+						 Box(modifier = Modifier
+							 .background(Color.Blue)
+							 .size(20.dp)) {
+							 Text("3")
+						 }
+					 }
+				 }
+				 true -> {
+					 Row {
+						 Box(modifier = Modifier
+							 .background(Color.Red)
+							 .size(20.dp)) {
+							 Text("A")
+						 }
+						 Box(modifier = Modifier
+							 .background(Color.Magenta)
+							 .size(20.dp)) {
+							 Text("B")
+						 }
+						 Box(modifier = Modifier
+							 .background(Color.Blue)
+							 .size(20.dp)) {
+							 Text("C")
+						 }
+					 }
+				 }
+			 }
+		}
+	}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-	Part4chapter415Theme {
-		Greeting("Android")
+	Animation2Theme {
+		Animation2Ex()
+	}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RadioButtonWithTextPreview() {
+	Animation2Theme {
+		RadioButtonWithText(
+			text = "라디오 버튼",
+			color = Color.Red,
+			selected = true,
+			onClick = {}
+		)
+	}
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RadioButtonWithText(
+	text: String,
+	color: Color = Color.Black,
+	selected: Boolean,
+	onClick: () -> Unit
+) {
+	Row(
+		modifier = Modifier.selectable(
+			selected = selected,
+			onClick = onClick
+		),
+		verticalAlignment = Alignment.CenterVertically
+	) {
+		RadioButton(selected = selected, onClick = onClick)
+		Text(text = text, color = color)
 	}
 }
