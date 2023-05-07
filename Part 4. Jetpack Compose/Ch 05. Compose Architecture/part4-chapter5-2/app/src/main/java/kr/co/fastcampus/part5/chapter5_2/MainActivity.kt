fa7c1c2d1ebd20e4f4be65282da568b936289208
Hyuck <lhg1304@nexmore.co.kr>
2023-05-07 14:43:32 +0900

    part4-chapter5-2 ViewModel

--- Part 4. Jetpack Compose/Ch 05. Compose Architecture/part4-chapter5-2/app/src/main/java/kr/co/fastcampus/part5/chapter5_2/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 05. Compose Architecture/part4-chapter5-2/app/src/main/java/kr/co/fastcampus/part5/chapter5_2/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import kr.co.fastcampus.part5.chapter5_2.ui.theme.ViewModelTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,28 +36,15 @@ class MainActivity : ComponentActivity() {
 
 // 단계 2: `ViewModel`을 상속받은 `ToDoViewModel`을 만듭니다.
 // 첫 단계에서는 내용을 비워두고 시작합시다.
+class ToDoViewModel : ViewModel() {
+	val text = mutableStateOf("")
 
-// 단계 3: `TopLevel`의 파라미터로 `ToDoViewModel` 타입의
-// `viewModel`을 전달합니다. 기본 값은 `viewModel()`로 설정합시다.
-// 에러가 발생하면 아래의 `import` 문을 추가합니다.
-// `import androidx.lifecycle.viewmodel.compose.viewModel`
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TopLevel() {
-	// 단계 4: text, setText를 뷰 모델로 옮겨봅시다.
-	// 뷰 모델의 프로퍼티로 변경할 경우에는 destrunction (비구조화,
-	// 구조 분해)는 사용할 수 없으니 `by`를 써봅시다.
-	// `remember`는 제거해야 합니다.
-	val (text, setText) = remember { mutableStateOf("") }
-
-	// 단계 5: `toDoList`, `onSubmit`, `onEdit`, `onToggle`,
-	// `onDelete`를 모두 뷰 모델로 옮겨봅시다.
-	val toDoList = remember { mutableStateListOf<ToDoData>() }
+	val toDoList = mutableStateListOf<ToDoData>()
 
 	val onSubmit: (String) -> Unit = {
 		val key = (toDoList.lastOrNull()?.key ?: 0) + 1
 		toDoList.add(ToDoData(key, it))
-		setText("")
+		text.value = ""
 	}
 
 	val onEdit: (Int, String) -> Unit = { key, newText ->
@@ -72,24 +61,65 @@ fun TopLevel() {
 		val i = toDoList.indexOfFirst { it.key == key }
 		toDoList.removeAt(i)
 	}
+}
 
-	Scaffold {
-		Column(Modifier.padding(it)) {
+// 단계 3: `TopLevel`의 파라미터로 `ToDoViewModel` 타입의
+// `viewModel`을 전달합니다. 기본 값은 `viewModel()`로 설정합시다.
+// 에러가 발생하면 아래의 `import` 문을 추가합니다.
+// `import androidx.lifecycle.viewmodel.compose.viewModel`
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopLevel(viewModel: ToDoViewModel = viewModel()) {
+	// 단계 4: text, setText를 뷰 모델로 옮겨봅시다.
+	// 뷰 모델의 프로퍼티로 변경할 경우에는 destrunction (비구조화,
+	// 구조 분해)는 사용할 수 없으니 `by`를 써봅시다.
+	// `remember`는 제거해야 합니다.
+//	val (text, setText) = remember { mutableStateOf("") }
+
+	// 단계 5: `toDoList`, `onSubmit`, `onEdit`, `onToggle`,
+	// `onDelete`를 모두 뷰 모델로 옮겨봅시다.
+//	val toDoList = remember { mutableStateListOf<ToDoData>() }
+//
+//	val onSubmit: (String) -> Unit = {
+//		val key = (toDoList.lastOrNull()?.key ?: 0) + 1
+//		toDoList.add(ToDoData(key, it))
+//		viewModel.text.value = ""
+//	}
+//
+//	val onEdit: (Int, String) -> Unit = { key, newText ->
+//		val i = toDoList.indexOfFirst { it.key == key }
+//		toDoList[i] = toDoList[i].copy(text = newText)
+//	}
+//
+//	val onToggle: (Int, Boolean) -> Unit = { key, checked ->
+//		val i = toDoList.indexOfFirst { it.key == key }
+//		toDoList[i] = toDoList[i].copy(done = checked)
+//	}
+//
+//	val onDelete: (Int) -> Unit = { key ->
+//		val i = toDoList.indexOfFirst { it.key == key }
+//		toDoList.removeAt(i)
+//	}
+
+	Scaffold { paddingValues ->
+		Column(Modifier.padding(paddingValues)) {
 			ToDoInput(
-				text = text,
-				onTextChange = setText,
-				onSubmit = onSubmit
+				text = viewModel.text.value,
+				onTextChange = {
+					viewModel.text.value = it
+				},
+				onSubmit = viewModel.onSubmit
 			)
 			LazyColumn {
 				items(
-					items = toDoList,
+					items = viewModel.toDoList,
 					key = { it.key }
 				) { toDoData ->
 					ToDo(
 						toDoData = toDoData,
-						onEdit = onEdit,
-						onToggle = onToggle,
-						onDelete = onDelete
+						onEdit = viewModel.onEdit,
+						onToggle = viewModel.onToggle,
+						onDelete = viewModel.onDelete
 					)
 				}
 			}
