fc95bd7a673fbdef7a4439d3edea45aa1077535f
Hyuck <lhg1304@nexmore.co.kr>
2023-05-07 11:41:37 +0900

    part4-chapter4-17 부수효과

--- Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-17/app/src/main/java/kr/co/fastcampus/part4/chapter4_17/MainActivity.kt
+++ Part 4. Jetpack Compose/Ch 03. Compose Component/part4-chapter4-17/app/src/main/java/kr/co/fastcampus/part4/chapter4_17/MainActivity.kt
@@ -1,15 +1,21 @@
 package kr.co.fastcampus.part4.chapter4_17
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.LifecycleOwner
 import kr.co.fastcampus.part4.chapter4_17.ui.theme.EffectTheme
 
@@ -33,13 +39,16 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EffectEx(lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current) {
-//	val scaffoldState = rememberScaffoldState()
+	val snackbarHostState = remember { SnackbarHostState() }
 
 	// 단계 1: `LaunchedEffect`을 이용해서 스낵바를 이용해 봅시다.
 	// 파라미터에는 `scaffoldState.snackbarHostState`를 전달합시다.
 	// "헬로 컴포즈"라고 출력합시다.
 	// `LaunchedEffect`는 `CouroutineScope`를 만들기 때문에 스코프를 별도로
 	// 만들 필요는 없습니다.
+	LaunchedEffect(snackbarHostState) {
+		snackbarHostState.showSnackbar("헬로 컴포즈. 패스트캠퍼스")
+	}
 
 	// 단계 2: `DisposableEffect`를 호출하고 파리미터로 `lifecycleOwner`를
 	// 전달합니다.
@@ -50,7 +59,41 @@ fun EffectEx(lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current) {
 	// 블록 내에서 `lifecycleOwner.lifecycle.addObserver`로 옵저버를 추가하고
 	// onDispose에서 옵저버를 제거합니다.
 
+	DisposableEffect(lifecycleOwner) {
+		val observer = LifecycleEventObserver { _, event ->
+			// SAM: Single Abstract Method
+			when (event) {
+				Lifecycle.Event.ON_START -> {
+					Log.d("이펙트", "ON_START")
+				}
+				Lifecycle.Event.ON_STOP -> {
+					Log.d("이펙트", "ON_STOP")
+				}
+				Lifecycle.Event.ON_PAUSE -> {
+					Log.d("이펙트", "ON_PAUSE")
+				}
+				Lifecycle.Event.ON_RESUME -> {
+					Log.d("이펙트", "ON_RESUME")
+				}
+				else -> {
+					Log.d("이펙트", "그외!")
+				}
+			}
+		}
+
+		lifecycleOwner.lifecycle.addObserver(observer)
+
+		onDispose {
+			lifecycleOwner.lifecycle.removeObserver(observer)
+		}
+	}
+
 	Scaffold(
+		snackbarHost = {
+			SnackbarHost(
+				hostState = snackbarHostState
+			)
+		}
 	) {
 		Surface(modifier = Modifier.padding(it)) {
 
