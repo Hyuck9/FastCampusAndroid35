1f8d1d8127b9b5c8d5b28c2297bbf141e27a2aef
Hyuck <lhg1304@nexmore.co.kr>
2023-05-11 01:57:52 +0900

    07. 영화 카테고리 만들기

--- Part 4plus. Jetpack Compose Intensive practice/Ch 03 Compose Practical uses/MovieApp/app/src/main/java/kr/co/fastcampus/part4plus/movieapp/ui/components/movie/MovieItem.kt
+++ Part 4plus. Jetpack Compose Intensive practice/Ch 03 Compose Practical uses/MovieApp/app/src/main/java/kr/co/fastcampus/part4plus/movieapp/ui/components/movie/MovieItem.kt
@@ -3,14 +3,18 @@ package kr.co.fastcampus.part4plus.movieapp.ui.components.movie
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kr.co.fastcampus.part4plus.movieapp.R
 
 private val CARD_WIDTH = 150.dp
 
@@ -37,6 +41,14 @@ fun MovieItem() {
 			modifier = Modifier.padding(vertical = 10.dp),
 			verticalAlignment = Alignment.CenterVertically
 		) {
+			Icon(
+				modifier = Modifier
+					.padding(4.dp)
+					.size(12.dp),
+				imageVector = ImageVector.vectorResource(id = R.drawable.ic_rating),
+				tint = Color.Black.copy(alpha = 0.5f),
+				contentDescription = "rating icon"
+			)
 			Text(text = "5.0")
 		}
 	}
