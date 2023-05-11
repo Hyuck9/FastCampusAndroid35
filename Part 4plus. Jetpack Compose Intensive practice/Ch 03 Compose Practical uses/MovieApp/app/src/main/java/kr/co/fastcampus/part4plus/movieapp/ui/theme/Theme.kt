016fedcbcf94c545c2e270884cd0b002bd199429
Hyuck <lhg1304@nexmore.co.kr>
2023-05-11 20:56:33 +0900

    09. 테마 - 타이포그래피, 쉐이프 설정

--- Part 4plus. Jetpack Compose Intensive practice/Ch 03 Compose Practical uses/MovieApp/app/src/main/java/kr/co/fastcampus/part4plus/movieapp/ui/theme/Theme.kt
+++ Part 4plus. Jetpack Compose Intensive practice/Ch 03 Compose Practical uses/MovieApp/app/src/main/java/kr/co/fastcampus/part4plus/movieapp/ui/theme/Theme.kt
@@ -2,16 +2,20 @@ package kr.co.fastcampus.part4plus.movieapp.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Shapes
+import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.staticCompositionLocalOf
-import kr.co.fastcampus.part4plus.movieapp.ui.theme.color.*
+import kr.co.fastcampus.part4plus.movieapp.ui.theme.color.ColorSet
 
 private val LocalColors = staticCompositionLocalOf { ColorSet.Red.LightColors }
 
 @Composable
 fun MovieAppTheme(
-	myColors: ColorSet,
+	myColors: ColorSet = ColorSet.Red,
+	typography: Typography = Typography,
+	shapes: Shapes = Shapes,
 	darkTheme: Boolean = isSystemInDarkTheme(),
 	content: @Composable () -> Unit
 ) {
@@ -24,8 +28,8 @@ fun MovieAppTheme(
 	CompositionLocalProvider(LocalColors provides colors ) {
 		MaterialTheme(
 			colors = colors.material,
-			typography = Typography,
-			shapes = Shapes,
+			typography = typography,
+			shapes = shapes,
 			content = content
 		)
 	}
