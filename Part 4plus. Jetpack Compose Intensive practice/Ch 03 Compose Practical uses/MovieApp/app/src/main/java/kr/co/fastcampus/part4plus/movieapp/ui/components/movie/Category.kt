f55c69cf8a10cd4dc8325ca50b78f5c5bf330bd8
Hyuck <lhg1304@nexmore.co.kr>
2023-05-11 21:05:11 +0900

    10. 컴포넌트에 테마 적용하기

--- Part 4plus. Jetpack Compose Intensive practice/Ch 03 Compose Practical uses/MovieApp/app/src/main/java/kr/co/fastcampus/part4plus/movieapp/ui/components/movie/Category.kt
+++ Part 4plus. Jetpack Compose Intensive practice/Ch 03 Compose Practical uses/MovieApp/app/src/main/java/kr/co/fastcampus/part4plus/movieapp/ui/components/movie/Category.kt
@@ -4,11 +4,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kr.co.fastcampus.part4plus.movieapp.ui.theme.MovieAppTheme
+import kr.co.fastcampus.part4plus.movieapp.ui.theme.Paddings
 
 @Composable
 fun CategoryRow() {
@@ -41,7 +44,12 @@ fun CategoryRow() {
 fun CategoryTitle(titleName: String) {
 	Text(
 		text = "Action",
-		modifier = Modifier.padding(10.dp)
+		modifier = Modifier
+			.padding(
+				vertical = Paddings.large,
+				horizontal = Paddings.extra
+			),
+		style = MaterialTheme.typography.h5
 	)
 }
 
@@ -49,5 +57,7 @@ fun CategoryTitle(titleName: String) {
 @Preview
 @Composable
 fun CategoryRowPreview() {
-	CategoryRow()
+	MovieAppTheme {
+		CategoryRow()
+	}
 }
