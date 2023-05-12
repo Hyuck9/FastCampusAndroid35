9ec6f4ddf0cbb58408e75182bfe04d260780f6b1
Hyuck <lhg1304@nexmore.co.kr>
2023-05-12 18:30:05 +0900

    15. 영화피드 - DI와 레트로핏 설정

---
+++ Part 4plus. Intensive practice/Ch 03 Practical uses/MovieApp/app/src/main/java/kr/co/fastcampus/part4plus/movieapp/features/feed/presentation/viewmodel/FeedViewModel.kt
@@ -0,0 +1,11 @@
+package kr.co.fastcampus.part4plus.movieapp.features.feed.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class FeedViewModel(
+
+) : ViewModel() {
+
+}
