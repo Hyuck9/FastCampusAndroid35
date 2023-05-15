17fc9f7358015145daf96eb52775fe1cca4e8a8e
Hyuck <lhg1304@nexmore.co.kr>
2023-05-15 23:42:41 +0900

    RestaurantApp 초기 구현

--- Part 4plus. Intensive practice/Ch 04. Multi module/RestaurantApp/app/src/main/java/kr/co/fastcampus/part4plus/restaurantapp/MainActivity.kt
+++ Part 4plus. Intensive practice/Ch 04. Multi module/RestaurantApp/app/src/main/java/kr/co/fastcampus/part4plus/restaurantapp/MainActivity.kt
@@ -1,43 +1,13 @@
 package kr.co.fastcampus.part4plus.restaurantapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import kr.co.fastcampus.part4plus.restaurantapp.ui.theme.RestaurantAppTheme
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : ComponentActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContent {
-			RestaurantAppTheme {
-				// A surface container using the 'background' color from the theme
-				Surface(
-					modifier = Modifier.fillMaxSize(),
-					color = MaterialTheme.colors.background
-				) {
-					Greeting("Android")
-				}
-			}
-		}
-	}
-}
-
-@Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-	RestaurantAppTheme {
-		Greeting("Android")
+		setContentView(R.layout.activity_main)
 	}
 }
