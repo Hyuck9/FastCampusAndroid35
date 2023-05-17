0c7b89aae5a23397a3dabe1057ae2b039f0f9185
Hyuck <lhg1304@nexmore.co.kr>
2023-05-18 02:33:29 +0900

    06. Main화면 탭 구성하기

--- Part 5/FinalShop/presentation/src/main/java/fastcampus/part5/chapter2/ui/MainActivity.kt
+++ Part 5/FinalShop/presentation/src/main/java/fastcampus/part5/chapter2/ui/MainActivity.kt
@@ -8,10 +8,7 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import fastcampus.part5.chapter2.ui.theme.FinalShopTheme
 import fastcampus.part5.chapter2.viewmodel.TempViewModel
@@ -19,11 +16,10 @@ import fastcampus.part5.chapter2.viewmodel.TempViewModel
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-	private val viewModel: TempViewModel by viewModels()
+//	private val viewModel: TempViewModel by viewModels()
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		Toast.makeText(this, "TempValue=${viewModel.getTempModel().name}", Toast.LENGTH_SHORT).show()
 
 		setContent {
 			FinalShopTheme {
@@ -32,22 +28,9 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colors.background
 				) {
-					Greeting("Android")
+					MainScreen()
 				}
 			}
 		}
-	}
-}
-
-@Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-	FinalShopTheme {
-		Greeting("Android")
 	}
 }
