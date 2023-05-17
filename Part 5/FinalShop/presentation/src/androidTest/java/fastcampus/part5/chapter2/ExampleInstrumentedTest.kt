3dd9d7446f503595f1a5a2b2f901a92e813ad6f7
Hyuck <lhg1304@nexmore.co.kr>
2023-05-18 01:27:48 +0900

    presentation module 추가

--- Part 5/FinalShop/presentation/src/androidTest/java/fastcampus/part5/chapter2/ExampleInstrumentedTest.kt
+++ Part 5/FinalShop/presentation/src/androidTest/java/fastcampus/part5/chapter2/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
 	fun useAppContext() {
 		// Context of the app under test.
 		val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-		assertEquals("fastcampus.part5.chapter2", appContext.packageName)
+		assertEquals("fastcampus.part5.chapter2.test", appContext.packageName)
 	}
 }
