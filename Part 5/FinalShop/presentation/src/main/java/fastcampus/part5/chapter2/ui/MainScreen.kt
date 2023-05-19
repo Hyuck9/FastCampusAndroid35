26f91d5522a8f27300ad4f0ebe7c166398045504
Hyuck <lhg1304@nexmore.co.kr>
2023-05-19 22:15:34 +0900

    07. 홈 화면 상단 검색바 구성하기

--- Part 5/FinalShop/presentation/src/main/java/fastcampus/part5/chapter2/ui/MainScreen.kt
+++ Part 5/FinalShop/presentation/src/main/java/fastcampus/part5/chapter2/ui/MainScreen.kt
@@ -2,24 +2,29 @@ package fastcampus.part5.chapter2.ui
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import fastcampus.part5.chapter2.R
 import fastcampus.part5.chapter2.ui.theme.FinalShopTheme
+import fastcampus.part5.chapter2.viewmodel.MainViewModel
 
-sealed class MainNavigationItem(var route: String, var name: String) {
-	object Main : MainNavigationItem("Main", "Main")
-	object Category : MainNavigationItem("Category", "Category")
-	object MyPage : MainNavigationItem("MyPage", "MyPage")
+sealed class MainNavigationItem(var route: String, val icon: ImageVector, var name: String) {
+	object Main : MainNavigationItem("Main", Icons.Filled.Home, "Main")
+	object Category : MainNavigationItem("Category", Icons.Filled.Star, "Category")
+	object MyPage : MainNavigationItem("MyPage", Icons.Filled.AccountBox,"MyPage")
 }
 
 @Preview(showBackground = true)
@@ -32,10 +37,14 @@ fun DefaultPreview() {
 
 @Composable
 fun MainScreen() {
+	val viewModel = hiltViewModel<MainViewModel>()
 	val scaffoldState = rememberScaffoldState()
 	val navController = rememberNavController()
 	
 	Scaffold(
+		topBar = {
+			Header(viewModel)
+		},
 		scaffoldState = scaffoldState,
 		bottomBar = {
 			MainBottomNavigationBar(navController)
@@ -49,6 +58,20 @@ fun MainScreen() {
 }
 
 @Composable
+fun Header(viewModel: MainViewModel) {
+	TopAppBar(
+		title = { Text(text = "My App") },
+		actions =  {
+			IconButton(onClick = {
+				viewModel.openSearchForm()
+			}) {
+				Icon(Icons.Filled.Search, "SearchIcon")
+			}
+		}
+	)
+}
+
+@Composable
 fun MainBottomNavigationBar(navController: NavHostController) {
 	val bottomNavigationItems = listOf(
 		MainNavigationItem.Main,
@@ -56,10 +79,7 @@ fun MainBottomNavigationBar(navController: NavHostController) {
 		MainNavigationItem.MyPage
 	)
 
-	BottomNavigation(
-		backgroundColor = Color(0xffff0000),
-		contentColor = Color(0xff00ff00)
-	) {
+	BottomNavigation {
 		val navBackStackEntry by navController.currentBackStackEntryAsState()
 		val currentRoute = navBackStackEntry?.destination?.route
 
@@ -67,7 +87,7 @@ fun MainBottomNavigationBar(navController: NavHostController) {
 			BottomNavigationItem(
 				icon = {
 					Icon(
-						painter = painterResource(id = R.drawable.ic_launcher_foreground),
+						imageVector = item.icon,
 						contentDescription = item.route
 					)
 				},
