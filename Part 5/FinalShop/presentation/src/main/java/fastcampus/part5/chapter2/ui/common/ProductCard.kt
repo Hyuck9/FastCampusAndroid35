a9a385c3e85b07746bad45d187608703fb821eff
Hyuck <lhg1304@nexmore.co.kr>
2023-05-20 03:01:58 +0900

    12. 홈 화면 상품 리스트 구성하기 - 2

--- Part 5/FinalShop/presentation/src/main/java/fastcampus/part5/chapter2/ui/common/ProductCard.kt
+++ Part 5/FinalShop/presentation/src/main/java/fastcampus/part5/chapter2/ui/common/ProductCard.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -37,17 +38,17 @@ fun ProductCard(
 		Column(
 			modifier = Modifier
 				.fillMaxWidth()
-				.height(320.dp)
 				.padding(10.dp),
 			verticalArrangement = Arrangement.Center,
 			horizontalAlignment = Alignment.Start
 		) {
 			Image(
 				painter = painterResource(id = R.drawable.product_image),
+				contentScale = ContentScale.Crop,
 				contentDescription = "description",
 				modifier = Modifier
 					.fillMaxWidth()
-					.wrapContentWidth(Alignment.End)
+					.aspectRatio(1f)
 			)
 			Text(
 				fontSize = 14.sp,
@@ -70,28 +71,21 @@ private fun Price(product: Product) {
 			Text(
 				fontSize = 18.sp,
 				fontWeight = FontWeight.Bold,
-				text = "${product.price.originPrice}"
+				text = "${product.price.originPrice}원"
 			)
 		}
 		SalesStatus.ON_DISCOUNT -> {
 			Text(
 				fontSize = 14.sp,
-				text = "${product.price.originPrice}",
+				text = "${product.price.originPrice}원",
 				style = TextStyle(textDecoration = TextDecoration.LineThrough)
 			)
-			Row {
-				Text(
-					fontSize = 18.sp,
-					fontWeight = FontWeight.Bold,
-					text = "할인가: "
-				)
-				Text(
-					fontSize = 18.sp,
-					fontWeight = FontWeight.Bold,
-					color = Purple200,
-					text = "${product.price.finalPrice}"
-				)
-			}
+			Text(
+				fontSize = 18.sp,
+				fontWeight = FontWeight.Bold,
+				color = Purple200,
+				text = "${product.price.finalPrice}원"
+			)
 		}
 		SalesStatus.SOLD_OUT -> {
 			Text(
--- Part 5/FinalShop/presentation/src/main/java/fastcampus/part5/chapter2/ui/main/MainComposable.kt
+++ Part 5/FinalShop/presentation/src/main/java/fastcampus/part5/chapter2/ui/main/MainComposable.kt
@@ -1,6 +1,7 @@
 package fastcampus.part5.chapter2.ui.main
 
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -10,7 +11,9 @@ import fastcampus.part5.chapter2.viewmodel.MainViewModel
 @Composable
 fun MainInsideScreen(viewModel: MainViewModel) {
 	val productList by viewModel.productList.collectAsState(initial = listOf())
-	LazyColumn {
+	val columnCount by viewModel.columCount.collectAsState()
+
+	LazyVerticalGrid(columns = GridCells.Fixed(columnCount)) {
 		items(productList.size) {
 			ProductCard(product = productList[it]) {
 				// TODO: 상세 화면 개발 시 추가
