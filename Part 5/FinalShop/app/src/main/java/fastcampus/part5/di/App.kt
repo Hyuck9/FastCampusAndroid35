108552ac9cd393e92491aec2a7bbd188ca644f0f
Hyuck <lhg1304@nexmore.co.kr>
2023-05-21 18:19:51 +0900

    30. 카카오 로그인 연동하기

--- Part 5/FinalShop/app/src/main/java/fastcampus/part5/di/App.kt
+++ Part 5/FinalShop/app/src/main/java/fastcampus/part5/di/App.kt
@@ -1,7 +1,15 @@
 package fastcampus.part5.di
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+	override fun onCreate() {
+		super.onCreate()
+
+		KakaoSdk.init(this,"a3add53dfca8288cd175bde0b3b88ec3")
+	}
+}
--- Part 5/FinalShop/presentation/src/main/java/fastcampus/part5/chapter2/viewmodel/MainViewModel.kt
+++ Part 5/FinalShop/presentation/src/main/java/fastcampus/part5/chapter2/viewmodel/MainViewModel.kt
@@ -36,15 +36,15 @@ class MainViewModel @Inject constructor(
 		NavigationUtils.navigate(navHostController, NavigationRouteName.SEARCH)
 	}
 
-	fun signInGoogle(accountInfo: AccountInfo) {
+	fun signIn(accountInfo: AccountInfo) {
 		viewModelScope.launch {
-			accountUseCase.signInGoogle(accountInfo)
+			accountUseCase.signIn(accountInfo)
 		}
 	}
 
-	fun signOutGoogle() {
+	fun signOut() {
 		viewModelScope.launch {
-			accountUseCase.signOutGoogle()
+			accountUseCase.signOut()
 		}
 	}
 
