be40af270bef0a4991d5fa747f87803a378a809c
Hyuck <lhg1304@nexmore.co.kr>
2023-06-11 22:01:23 +0900

    49. 구글 광고 붙이기 - 2

--- Part 5/FinalShop/app/src/main/java/fastcampus/part5/di/App.kt
+++ Part 5/FinalShop/app/src/main/java/fastcampus/part5/di/App.kt
@@ -1,6 +1,7 @@
 package fastcampus.part5.di
 
 import android.app.Application
+import com.google.android.gms.ads.MobileAds
 import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
@@ -11,5 +12,6 @@ class App : Application() {
 		super.onCreate()
 
 		KakaoSdk.init(this,"a3add53dfca8288cd175bde0b3b88ec3")
+		MobileAds.initialize(this)
 	}
 }
--- Part 5/FinalShop/presentation/src/main/java/fastcampus/part5/chapter2/ui/main/MainHomeScreen.kt
+++ Part 5/FinalShop/presentation/src/main/java/fastcampus/part5/chapter2/ui/main/MainHomeScreen.kt
@@ -1,12 +1,20 @@
 package fastcampus.part5.chapter2.ui.main
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavHostController
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
 import fastcampus.part5.chapter2.model.*
 import fastcampus.part5.chapter2.ui.component.*
 import fastcampus.part5.chapter2.viewmodel.MainViewModel
@@ -16,6 +24,10 @@ import fastcampus.part5.domain.model.ModelType
 fun MainHomeScreen(navController: NavHostController, viewModel: MainViewModel) {
 	val modelList by viewModel.modelList.collectAsState(initial = listOf())
 	val columnCount by viewModel.columCount.collectAsState()
+
+	val testId = "ca-app-pub-3940256099942544/6300978111"
+	val adId = "ca-app-pub-3897260489773203/2972373963"
+	val adRequest = AdRequest.Builder().build()
 
 	LazyVerticalGrid(columns = GridCells.Fixed(columnCount)) {
 		items(
@@ -35,6 +47,19 @@ fun MainHomeScreen(navController: NavHostController, viewModel: MainViewModel) {
 			}
 		}
 	}
+
+	AndroidView(modifier = Modifier
+		.fillMaxWidth()
+		.height(50.dp),
+		factory = {
+			AdView(it).apply {
+				setAdSize(AdSize.BANNER)
+				adUnitId = testId
+				loadAd(adRequest)
+			}
+		}, update = {
+			it.loadAd(adRequest)
+		})
 }
 
 private fun getSpanCountByType(type: ModelType, defaultColumnCount: Int): Int {
