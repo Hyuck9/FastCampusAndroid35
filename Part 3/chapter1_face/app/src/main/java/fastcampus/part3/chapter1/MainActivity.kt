c4d3d782c9f405c1c3848b598e8129db6513491b
Hyuck <lhg1304@nexmore.co.kr>
2023-06-18 05:53:40 +0900

    part3-chapter1 - 09. 얼굴 인식 모듈 연동

--- Part 3/chapter1_face/app/src/main/java/fastcampus/part3/chapter1/MainActivity.kt
+++ Part 3/chapter1_face/app/src/main/java/fastcampus/part3/chapter1/MainActivity.kt
@@ -1,22 +1,68 @@
 package fastcampus.part3.chapter1
 
+import android.graphics.PointF
+import android.graphics.RectF
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.SizeF
 import androidx.core.view.isVisible
+import androidx.transition.TransitionManager
 import fastcampus.part3.chapter1.databinding.ActivityMainBinding
+import fastcampus.part3.face_recognition.Camera
+import fastcampus.part3.face_recognition.recognition.FaceAnalyzerListener
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FaceAnalyzerListener {
 
 	private lateinit var binding: ActivityMainBinding
+
+	private val camera = Camera(this)
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		binding = ActivityMainBinding.inflate(layoutInflater).apply {
 			setContentView(root)
+			setProgressText("시작하기를 눌러주세요.")
+			camera.initCamera(cameraLayout, this@MainActivity)
 
 			startDetectButton.setOnClickListener {
 				it.isVisible = false
+				camera.startFaceDetect()
+				setProgressText("얼굴을 보여주세요.")
 			}
 		}
+
+	}
+
+	override fun detect() {}
+	override fun stopDetect() {
+		camera.stopFaceDetect()
+		reset()
+	}
+	override fun notDetect() {}
+	override fun detectProgress(progress: Float, message: String) {
+		setProgressText(message)
+	}
+	override fun faceSize(rectF: RectF, sizeF: SizeF, pointF: PointF) {}
+
+
+	override fun onRequestPermissionsResult(
+		requestCode: Int,
+		permissions: Array<out String>,
+		grantResults: IntArray
+	) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+		camera.onRequestPermissionsResult(requestCode, permissions, grantResults)
+	}
+
+	private fun reset() {
+		binding.startDetectButton.isVisible = true
+	}
+
+	/**
+	 * 텍스트가 변경될 때마다 자동으로 애니메이션 처리
+	 */
+	private fun setProgressText(text: String) {
+		TransitionManager.beginDelayedTransition(binding.root)
+		binding.progressTextView.text = text
 	}
 }
