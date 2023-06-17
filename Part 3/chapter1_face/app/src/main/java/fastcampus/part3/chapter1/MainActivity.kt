474cb456a7db581c838c1436ab5711f6cc21d8f7
Hyuck <lhg1304@nexmore.co.kr>
2023-06-18 04:52:36 +0900

    part3-chapter1 - 03. Main UI 그리기

--- Part 3/chapter1_face/app/src/main/java/fastcampus/part3/chapter1/MainActivity.kt
+++ Part 3/chapter1_face/app/src/main/java/fastcampus/part3/chapter1/MainActivity.kt
@@ -2,10 +2,21 @@ package fastcampus.part3.chapter1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
+import fastcampus.part3.chapter1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+	private lateinit var binding: ActivityMainBinding
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		binding = ActivityMainBinding.inflate(layoutInflater).apply {
+			setContentView(root)
+
+			startDetectButton.setOnClickListener {
+				it.isVisible = false
+			}
+		}
 	}
 }
