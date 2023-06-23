db7451206e5a832eb306f9c2bd1a149cb760bba6
Hyuck <lhg1304@nexmore.co.kr>
2023-06-23 13:35:34 +0900

    part3-chapter2 - 08. 휴대폰 본인 인증 UI 구성 2

--- Part 3/chapter2_finance/app/src/main/java/fastcampus/part3/chapter2/IdentityInputActivity.kt
+++ Part 3/chapter2_finance/app/src/main/java/fastcampus/part3/chapter2/IdentityInputActivity.kt
@@ -1,8 +1,15 @@
 package fastcampus.part3.chapter2
 
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.core.widget.doAfterTextChanged
 import fastcampus.part3.chapter2.databinding.ActivityIdentityInputBinding
+import fastcampus.part3.chapter2.util.ViewUtil.hideKeyboard
+import fastcampus.part3.chapter2.util.ViewUtil.setOnEditorActionListener
+import fastcampus.part3.chapter2.util.ViewUtil.showKeyboard
+import fastcampus.part3.chapter2.util.ViewUtil.showKeyboardDelay
 
 class IdentityInputActivity : AppCompatActivity() {
 
@@ -13,5 +20,50 @@ class IdentityInputActivity : AppCompatActivity() {
 		binding = ActivityIdentityInputBinding.inflate(layoutInflater)
 		setContentView(binding.root)
 		binding.view = this
+		initView()
+		binding.nameEdit.showKeyboardDelay()
+	}
+
+	private fun initView() {
+		with(binding) {
+			nameEdit.setOnEditorActionListener(EditorInfo.IME_ACTION_NEXT) {
+				birthdayLayout.isVisible = true
+				birthdayEdit.showKeyboard()
+			}
+
+			birthdayEdit.doAfterTextChanged {
+				if (birthdayEdit.length() > 7) {
+					genderLayout.isVisible = true
+					birthdayEdit.hideKeyboard()
+				}
+			}
+
+			genderChipGroup.setOnCheckedStateChangeListener { group, checkedIds ->
+				if (!telecomLayout.isVisible) {
+					telecomLayout.isVisible = true
+				}
+			}
+
+			telecomChipGroup.setOnCheckedStateChangeListener { group, checkedIds ->
+				if (!phoneLayout.isVisible) {
+					phoneLayout.isVisible = true
+					phoneEdit.showKeyboard()
+				}
+			}
+
+			phoneEdit.doAfterTextChanged {
+				if (phoneEdit.length() > 10) {
+					confirmButton.isVisible = true
+					phoneEdit.hideKeyboard()
+				}
+			}
+
+			phoneEdit.setOnEditorActionListener(EditorInfo.IME_ACTION_DONE) {
+				if (phoneEdit.length() > 9) {
+					confirmButton.isVisible = true
+					phoneEdit.hideKeyboard()
+				}
+			}
+		}
 	}
 }
