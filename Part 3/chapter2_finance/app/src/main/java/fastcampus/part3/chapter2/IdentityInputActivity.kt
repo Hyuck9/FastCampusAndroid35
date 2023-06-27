3d136b357d64c292728eb6abd7d3e6a40cfeb71f
Hyuck <lhg1304@nexmore.co.kr>
2023-06-27 10:44:09 +0900

    part3-chapter2 - 10. 휴대폰 본인 인증 UI 화면 구성 (인증번호 입력) 1

--- Part 3/chapter2_finance/app/src/main/java/fastcampus/part3/chapter2/IdentityInputActivity.kt
+++ Part 3/chapter2_finance/app/src/main/java/fastcampus/part3/chapter2/IdentityInputActivity.kt
@@ -1,5 +1,6 @@
 package fastcampus.part3.chapter2
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
 import androidx.appcompat.app.AppCompatActivity
@@ -117,6 +118,8 @@ class IdentityInputActivity : AppCompatActivity() {
 			binding.phoneLayout.error = "전화번호 형식이 다릅니다."
 			return
 		}
+
+		startActivity(Intent(this, VerifyOtpActivity::class.java))
 	}
 
 	private fun validName() = !binding.nameEdit.text.isNullOrBlank()
